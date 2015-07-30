f5295685f9e388e4353f83624e5aaf64965a5b74
Andrei-George Hondrari <andrei.hondrari@spyhce.com>
2015-07-30 15:09:24 +0300

    Added adapter pattern.

---
+++ design_patterns/adapter.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+from utils import p 
+
+class CompliantInstance(object):
+
+    def __init__(self):
+        self.x = int()
+        self.y = int()
+
+    def doXY(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Adaptee(object):
+
+    def __init__(self):
+        pass
+
+    def doX(self, X):
+        self.x = X
+
+    def doY(self, Y):
+        self.y = Y
+
+
+class AdapteeAdapter(object):
+
+    def __init__(self):
+        self.adaptee = Adaptee()
+
+    def doXY(self, x, y):
+        self.adaptee.doX(x)
+        self.adaptee.doY(y)
+
+
+class Client(object):
+
+    C_X = 33
+    C_Y = 44
+
+    def __init__(self, usedInstance):
+        self.used = usedInstance
+
+    def doIt(self):
+        self.used.doXY(self.C_X, self.C_Y)
+
+
+a = AdapteeAdapter()
+c = Client(a)
+c.doIt()
+
+print a.adaptee.x
+print a.adaptee.y
+
+# TODO: make adapter inherited
+# TODO: make adapter-adaptee composition with __dict__ adaptation from adapter to adaptee
---
+++ design_patterns/utils.py
@@ -0,0 +1,2 @@
+def p(m):
+	print "\n###", str(m)
