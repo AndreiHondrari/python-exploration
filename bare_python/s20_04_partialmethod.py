10e87e449dd1bf02fa4a7d9f67f18e3ef3193d44
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-05 19:46:28 +0300

    Explore functools.partialmethod.

---
+++ bare_python/s20_04_partialmethod.py
@@ -0,0 +1,26 @@
+#!python
+
+from functools import partialmethod
+
+
+class A:
+
+    def __init__(self, msg):
+        self._msg = msg
+
+    def show_message(self):
+        print(self._msg)
+
+    def update_message(self, msg):
+        self._msg = msg
+
+    default_message = partialmethod(update_message, "DEFAULT_MESSAGE_RIGHT_HERE_RIGHT_NOW")
+
+a = A("Anne had three apples.")
+a.show_message()
+
+a.update_message("Anne had no more apples!")
+a.show_message()
+
+a.default_message()
+a.show_message()
