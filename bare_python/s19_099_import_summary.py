28c8b34e0545db6639a0871da5c03a9fc00d7021
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-05 16:12:49 +0300

    __import__ summary.

--- bare_python/s19_099_import_summary.py
+++ bare_python/s19_099_import_summary.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 
+# import pack1
+__import__("pack1")
+
+# import pack1 --> only specific module available
+pkg = __import__("pack1.pmod2")
+pkg = __import__("pack1", fromlist=('pmod2',))  # similar
+# pkg.othermod will not work only pmod2
+
+# from pack1.pmod1 import pmod1_something
+# __import__("pack1.pmod1.pmod1_something") will NOT work!
+_temp = __import__("pack1.pmod1", fromlist=("pmod1",))
+pmod1_something = _temp.pmod1_something
+
+# from pack1 import pmod1
+__import__("pack1.pmod1", fromlist=('pmod1',))
+
+# import mod1
+__import__("mod1")
+
+# from mod1 import something
+_temp = __import__("mod1")
+something = _temp.something
