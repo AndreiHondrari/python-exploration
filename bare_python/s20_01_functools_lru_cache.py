b5b56b7c623bf11b688e0f53104ff049a9912144
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-05 19:23:29 +0300

    Further explore of LRU capabilities.

--- bare_python/s20_01_functools_lru_cache.py
+++ bare_python/s20_01_functools_lru_cache.py
@@ -7,7 +7,7 @@ from ut import p
 
 @lru_cache(maxsize=128)
 def some(x):
-    return sum(range(x))
+    return sum(range(x))  # --> a time consuming operation (with large x's)
 
 p("wait for it...")
 
@@ -31,3 +31,15 @@ start = time.time()
 some(A)  # --> A
 end = time.time()
 print(f"{end-start:.2f}s --> obviously faster this time because it used the cached value")
+
+p("Explore the cache info")
+print(some.cache_info())
+
+p("Clear the cache")
+some.cache_clear()
+
+p("fourth run with A")
+start = time.time()
+some(A)  # --> A
+end = time.time()
+print(f"{end-start:.2f}s --> this time slower as the first time because of the cache clear")
