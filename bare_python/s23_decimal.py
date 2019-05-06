e6799beba5524ddf27266ab4a0802510f7c9a128
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-06 16:11:09 +0300

    Explore decimal further.

--- bare_python/s23_decimal.py
+++ bare_python/s23_decimal.py
@@ -1,8 +1,31 @@
 #!python
+import copy
+from decimal import Decimal, getcontext, setcontext
 
-from decimal import *
+from ut import p
 
-D = Decimal
+initial_context = getcontext()
 
-print(1.1 + 2.2)
-print(D('1.1') + D('2.2') )
+# with initial context
+p("0.1 + 0.2 plain floats")
+print(0.1 + 0.2)
+
+p("Decimal('0.1') + Decimal('0.2') --> from strings")
+print(Decimal('0.1') + Decimal('0.2') )
+
+p(f"Decimal(0.1) + Decimal(0.2) --> from floats with precision: {initial_context.prec}")
+print(Decimal(0.1) + Decimal(0.2) )
+
+p(f"Decimal((0, (1,), -1)) + Decimal((0, (2,), -1))")
+print(Decimal((0, (1,), -1)) + Decimal((0, (2,), -1)))
+
+# with altered context
+altered_context = copy.deepcopy(initial_context)
+altered_context.prec = 2
+p(f"set new context with precision: {altered_context.prec}")
+setcontext(altered_context)
+
+p(f"alter context with precision: {altered_context.prec}")
+
+p(f"Decimal(0.1) + Decimal(0.2) --> from floats with precision: {initial_context.prec}")
+print(Decimal(0.1) + Decimal(0.2) )
