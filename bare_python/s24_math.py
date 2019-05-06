d131ce3535732862bed25de2397103c77ebba383
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-06 19:37:27 +0300

    Explore some math ops.

--- bare_python/s24_math.py
+++ bare_python/s24_math.py
@@ -1,3 +1,16 @@
 #!python
 
-from math import *
+import math
+
+from ut import p
+
+p("float constants")
+print(f"{math.nan}")
+print(f"{math.inf}")
+print(f"{-math.inf}")
+print(f"pi: {math.pi}")
+print(f"e: {math.e}")
+
+p("roundings")
+print(f"math.ceil(0.2): {math.ceil(0.2)}")
+print(f"math.floor(0.7): {math.floor(0.7)}")
