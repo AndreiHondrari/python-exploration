418f41954a7277837d9a4ae43caf6e67bb2f446f
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-07 16:33:31 +0300

    s34 Extensive exploration and additional information on __new__ and __init__.

--- bare_python/s34_01_py2_return_from_new_and_init.py
+++ bare_python/s34_01_py2_return_from_new_and_init.py
@@ -1,14 +1,27 @@
-#!python
+#!python2
+
+from __future__ import print_function
+
+import sys
+
+python_version = sys.version_info.major
+
+if python_version > 2:
+    print("\n\n!!! ATTENTION! You need to run this with Python 2 !!!\n")
+    sys.exit(1)
+
+
+#!/usr/bin/python
 
 print("\nreturn something specific from __new__ in new-style")
-class A:
+class A(object):
 
     def __new__(cls):
         print("A.__new__ called")
         return 22
 
     def __init__(self):
-        print("A.__init__ called" ) # is actually never called
+        print("A.__init__ called")  # is actually never called
 
     def __str__(self):
         return "THIS IS A"  # is actually never called
@@ -19,7 +32,7 @@ print("\nreturn something specific from __new__ in old-style")
 class B():
 
     def __new__(cls):
-        print("B.__new__ called" ) # is never called because __init__ is the constructor
+        print("B.__new__ called")  # is never called because __init__ is the constructor
         return 22
 
     def __init__(self):
@@ -43,7 +56,7 @@ class B2():
 print(B2())
 
 print("\nmimmic original __new__ in new-style")
-class C:
+class C(object):
 
     def __new__(cls):
         print("C.__new__ called")
@@ -58,48 +71,47 @@ class C:
 print(C())
 
 print("\nnot return anything from __new__ in new-style")
-class D:
+class D(object):
 
     def __new__(cls):
         print("D.__new__ called")
         pass  # do nothing
 
     def __init__(self):
-        print("D.__init__ called" ) # is actually never called
+        print("D.__init__ called")  # is actually never called
 
     def __str__(self):
         return "THIS IS D"  # is actually never called
 
 print(D())
 
-class Sample:
+class Sample(object):
     def __str__(self):
         return "SAMPLE"
 
 print("\nreturn different class instance from __new__ in new-style")
-class E:
+class E(object):
 
     def __new__(cls):
         print("E.__new__ called")
         return Sample()
 
     def __init__(self):
-        print("E.__init__ called" ) # is actually never called
+        print("E.__init__ called")  # is actually never called
 
     def __str__(self):
-        return "THIS IS E"  # is actually never called 
+        return "THIS IS E"  # is actually never called
 
 print(E())
 
 print("\nreturn from __init__ in new-style")
-class F:
+class F(object):
 
     def __init__(self):
-        print("F.__init__ called"  )
+        print("F.__init__ called")
     #     return 33 # TypeError: __init__ should return None
 
     def __str__(self):
-        return "THIS IS F"  # is actually never called 
+        return "THIS IS F"  # is actually never called
 
 print(F())
-
---
+++ bare_python/s34_02_py3_return_from_new_and_init.py
@@ -0,0 +1,96 @@
+#!python
+
+from __future__ import print_function
+
+import sys
+
+python_version = sys.version_info.major
+
+if python_version < 3:
+    print("\n\n!!! ATTENTION! You need to run this with Python 3 !!!\n")
+    sys.exit(1)
+
+
+print("\nreturn something specific from __new__")
+class A:
+
+    def __new__(cls):
+        print("A.__new__ called")
+        return 22
+
+    def __init__(self):
+        print("A.__init__ called" ) # is actually never called
+        # because 22.__init__ is called
+
+    def __str__(self):
+        return "THIS IS A"  # is actually never called
+        # because 22.__str__ is called
+
+print(A())
+
+print("\nmimmic original __new__")
+class B:
+
+    def __new__(cls):
+        print("B.__new__ called")
+        return super().__new__(cls)
+
+    def __init__(self):
+        print("B.__init__ called")
+
+    def __str__(self):
+        return "THIS IS B"
+
+print(B())
+
+print("\nnot return anything from __new__")
+class C:
+
+    def __new__(cls):
+        print("C.__new__ called")
+        pass  # do nothing
+
+    def __init__(self):
+        print("C.__init__ called" ) # is actually never called
+
+    def __str__(self):
+        return "THIS IS C"  # is actually never called
+
+print(C())
+
+class Sample:
+
+    def __init__(self):
+        print("Sample.__init__ called")
+
+    def __str__(self):
+        return "SAMPLE"
+
+print("\nreturn different class instance from __new__")
+class D:
+
+    def __new__(cls):
+        print("D.__new__ called")
+        return Sample()
+
+    def __init__(self):
+        print("D.__init__ called" ) # is actually never called
+        # because Sample.__init__ gets called
+
+    def __str__(self):
+        return "THIS IS D"  # is actually never called
+        # because Sample.__str__ gets called
+
+print(D())
+
+print("\nreturn from __init__")
+class E:
+
+    def __init__(self):
+        print("E.__init__ called"  )
+        return 33  # TypeError: __init__ should return None
+
+try:
+    print(E())
+except TypeError as e:
+    print(f"raised: {repr(e)}")
