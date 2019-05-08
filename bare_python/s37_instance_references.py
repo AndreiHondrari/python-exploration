35690275e6e46c716455fcfd4796fe2fa51e00aa
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-08 16:45:46 +0300

    s37 instance references explore.

---
+++ bare_python/s37_instance_references.py
@@ -0,0 +1,38 @@
+#!python
+
+from ut import p
+
+
+class A:
+
+    def __init__(self, x):
+        self._x = x
+
+    def __repr__(self):
+        return f"A(x={self._x})"
+
+a1 = A(100)
+p(f"instantiated a1 = {a1}")
+
+a2 = a1
+p(f"referenced instance a2 = a1")
+
+p("del a1")
+del a1
+
+
+p(f"attempt using a1")
+try:
+    a1
+except NameError as e:
+    print(f"raised: {repr(e)}")
+else:
+    print(f"could actually use a1: {a1}")
+
+p(f"attempt using a2")
+try:
+    a2
+except NameError as e:
+    print(f"raised: {repr(e)}")
+else:
+    print(f"could actually use a2: {a2}")
