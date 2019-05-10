222227cdb61028cdf51c30425b03b553a2c926b6
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-11 00:10:15 +0300

    databases make a simple sqlite3 connection.

---
+++ databases/simple_connection/s01_sqlite.py
@@ -0,0 +1,17 @@
+#!python
+
+
+import sqlite3
+
+conn = sqlite3.connect("some.db")
+
+c = conn.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS bla (x integer)")
+c.execute("INSERT INTO bla VALUES (10)")
+c.execute("INSERT INTO bla VALUES (20)")
+c.execute("INSERT INTO bla VALUES (30)")
+c.execute("INSERT INTO bla VALUES (40)")
+
+conn.commit()
+conn.close()
