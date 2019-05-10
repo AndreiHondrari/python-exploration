f3ff424ac4e8fbf0ee499323a4f05621e8f20369
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-10 14:59:44 +0300

    s39 and s40, dataclass and contextvar exploration.

---
+++ bare_python/s39_dataclass.py
@@ -0,0 +1,25 @@
+#!python
+
+from dataclasses import dataclass, field
+
+from ut import p
+
+
+@dataclass
+class A:
+    x: int
+    y: int
+    z: int = field(repr=False)
+
+
+p("A(10, 20, 30)")
+a1 = A(10, 20, 30)
+print(a1)
+
+p("A(z=10, y=20, x=30)")
+a2 = A(z=10, y=20, x=30)
+print(a2)
+
+p("A(10, y=20, z=30)")
+a3 = A(10, y=20, z=30)
+print(a3)
---
+++ bare_python/s40_contextvar.py
@@ -0,0 +1,25 @@
+#!python
+
+from contextvars import ContextVar, Context
+from typing import Any, NoReturn, List
+
+import contextvars
+
+name: ContextVar[Any] = ContextVar("name")
+contexts: List[Context] = list()
+
+
+def greet() -> NoReturn:
+    print(f"Hello {name.get()}")
+
+
+# Construct contexts and set the context variable name
+for first_name in ["Steve", "Dina", "Harry"]:
+    ctx = contextvars.copy_context()
+    ctx.run(name.set, first_name)
+    contexts.append(ctx)
+
+
+# Run greet function inside each context
+for ctx in reversed(contexts):
+    ctx.run(greet)
