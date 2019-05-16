c03cb5ae506a3971728cb39fbe3e6d1347b94aae
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-16 18:00:06 +0300

    tricks. XOR swap.

---
+++ tricks/s01_xor_variable_swap.py
@@ -0,0 +1,21 @@
+#!python
+
+from ut import p
+
+a = 5
+b = 9
+
+p("XOR variables swap")
+print(f"a: {a} {bin(a)}, b: {b} {bin(b)}\n")
+
+print("a = a XOR b (augment the information in a)")
+a = a ^ b
+print(f"a: {a} {bin(a)}, b: {b} {bin(b)}\n")
+
+print("b = a XOR b (deaugment in b)")
+b = a ^ b
+print(f"a: {a} {bin(a)}, b: {b} {bin(b)}\n")
+
+print("a = a XOR b (finally deaugment in a)")
+a = a ^ b
+print(f"a: {a} {bin(a)}, b: {b} {bin(b)}\n")
