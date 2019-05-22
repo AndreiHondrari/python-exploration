70de9e0acb914ca8841235935a8fe69910d95f92
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-22 17:03:24 +0300

    tricks. s02 Reverse number.

---
+++ tricks/s02_reverse_number.py
@@ -0,0 +1,26 @@
+#!python
+
+from ut import p
+
+
+def reverse_number(n: int) -> int:
+    reversed = 0
+    while n > 0:
+        last_digit = n % 10
+        reversed = reversed * 10 + last_digit
+        n = n // 10
+    return reversed
+
+
+p("Number reversing")
+
+
+def show_it(n: int) -> None:
+    print(f"{n} -> {reverse_number(n)}")
+
+
+show_it(1)
+show_it(10)
+show_it(100)
+show_it(98)
+show_it(12345)
