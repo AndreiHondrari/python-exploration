55ea8a5b9677f0db1bf16d5a4f352bba494b6a35
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-22 18:13:02 +0300

    tricks. s03 get digits

---
+++ tricks/s03_get_digits.py
@@ -0,0 +1,33 @@
+#!python
+
+from typing import List, Tuple
+
+from ut import p
+
+
+def get_digits(n: int) -> Tuple[int, List[int]]:
+    digits: List[int] = []
+    digits_count = 1
+    digits.append(n % 10)
+    n //= 10
+
+    while n != 0:
+        digits.append(n % 10)
+        n //= 10
+        digits_count += 1
+
+    return digits_count, digits[::-1]
+
+
+p("Get digits")
+
+
+def show_it(n: int) -> None:
+    print(f"{n} -> {get_digits(n)}")
+
+
+show_it(1)
+show_it(10)
+show_it(100)
+show_it(98)
+show_it(12345)
