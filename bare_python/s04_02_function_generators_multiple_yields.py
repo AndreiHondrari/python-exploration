c42e471fc2a12b97e78459ede1ba088fda886f42
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-29 14:32:55 +0300

    bare_python. s04. rename initial function_generators to s04_01 and create a multiple yield function in s04_02.

---
+++ bare_python/s04_02_function_generators_multiple_yields.py
@@ -0,0 +1,22 @@
+#!python
+
+from typing import Iterator
+
+
+def gen(n: int) -> Iterator[int]:
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+
+
+# return 0 -> raises SyntaxError
+# (can't have return and yield in a generator function)
+a = gen(4)
+print(a)
+
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))  # -> yields StopIteration
