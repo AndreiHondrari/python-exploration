3317011c78ca75ca1535b607b4a40a2a84ae00b4
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-12-13 01:37:54 +0200

    Explanation on receiver vs sender_conn

--- networking/socket/s01_basic_socket/receiver.py
+++ networking/socket/s01_basic_socket/receiver.py
@@ -1,4 +1,5 @@
 import socket
+import errno
 
 
 def main() -> None:
@@ -14,6 +15,13 @@ def main() -> None:
 
     (sender_conn, addr, ) = receiver.accept()
 
+    # receiver socket is used only for acquiring new connectiobs
+    try:
+        receiver.recv(1024)
+    except OSError as oerr:
+        print("Caught", str(oerr), errno.errorcode[oerr.errno])
+
+    # the socket created with the accept method is the way to communicate
     msg = sender_conn.recv(1024)
 
     print("RECV", msg)
