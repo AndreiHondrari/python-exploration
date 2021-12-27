49bc211915e0fc65095e58df046542a588449169
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-12-27 21:08:56 +0100

    Fix feedback port

---
+++ networking/socket/s01_05_tcp_feedback_port/receiver.py
@@ -0,0 +1,32 @@
+import socket
+
+
+def main() -> None:
+    print("Receiver start")
+    receiver = socket.socket(
+        socket.AF_INET,  # socket family
+        socket.SOCK_STREAM,  # socket type
+    )
+
+    HOST = "0.0.0.0"
+    PORT = 5678
+    receiver.bind((HOST, PORT,))
+    receiver.listen()
+
+    (sender_conn, addr, ) = receiver.accept()
+
+    # get something
+    msg = sender_conn.recv(1024)
+
+    # reply to it
+    sender_conn.send(b"I got your message !")
+
+    if sender_conn.fileno() > 0:
+        sender_conn.close()
+    receiver.close()
+
+    print("RECV", msg)
+
+
+if __name__ == '__main__':
+    main()
---
+++ networking/socket/s01_05_tcp_feedback_port/sender.py
@@ -0,0 +1,29 @@
+import socket
+
+
+def main() -> None:
+    print("Sender start")
+    sender = socket.socket(
+        socket.AF_INET,  # socket family
+        socket.SOCK_STREAM,  # socket type
+    )
+
+    HOST = "127.0.0.1"
+    PORT = 5678
+    FEEDBACK_PORT = 50_123
+    sender.bind(("0.0.0.0", FEEDBACK_PORT))
+    sender.connect((HOST, PORT,))
+
+    print("sending ...")
+    sender.send(b"hello other node!")
+
+    print("Wait for reply ...")
+    reply = sender.recv(1024)
+    print("REPLY:", reply)
+
+    sender.close()
+    print("DONE")
+
+
+if __name__ == '__main__':
+    main()
