b914cc92babb31f04e9c7f5f474079a96d5bbcd2
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-12-30 13:21:04 +0100

    Shared memory basic example

---
+++ input_output/shared_memory/s01_basic_shared_memory/process_a.py
@@ -0,0 +1,58 @@
+import time
+import random
+import functools
+
+from multiprocessing import shared_memory as shmem
+
+
+hprint = functools.partial(print, "\n#")
+
+
+def main() -> None:
+    print("PROCESS A - START")
+    some_shm = shmem.SharedMemory(
+        name="gandalf_12345",
+        create=True,
+        size=1
+    )
+
+    hprint("Writing values to shared memory")
+    try:
+        while True:
+            hprint("New write sequence")
+
+            # values generation
+            vals = [
+                random.randint(0, 255)
+                for _ in range(3)
+            ]
+            print("vals :", vals)
+
+            # values encoding
+            vals_bytes = bytearray(vals)
+            print("bytes:", vals_bytes)
+
+            # write encoded values to shared memory
+            for i, val_byte in enumerate(vals_bytes):
+                print("write", i, val_byte)
+                some_shm.buf[i] = val_byte
+
+            time.sleep(random.random())
+
+    except KeyboardInterrupt:
+        print("\nCtrl+C detected !")
+
+    hprint("Cleaning ...")
+    some_shm.close()
+    try:
+        some_shm.unlink()
+    except FileNotFoundError as fnf_err:
+        print("Was unable to unlink shared memory")
+        print(repr(fnf_err))
+    print("Cleaning done")
+
+    print("PROCESS A - END")
+
+
+if __name__ == "__main__":
+    main()
